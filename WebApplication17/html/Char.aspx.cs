673af519f55d772e692b62895b14627becd655fd
shaki <72961134+devshaki@users.noreply.github.com>
2022-12-28 15:19:52 +0200

    char works fully

--- WebApplication17/html/Char.aspx.cs
+++ WebApplication17/html/Char.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,8 +10,33 @@ namespace WebApplication17.html
 {
     public partial class Char : System.Web.UI.Page
     {
+        public string respond; 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.Form["encode"] != null)
+            {
+                string str = Request.Form["input"];
+                var charArray = str.ToCharArray();
+                foreach (int CharSet in charArray)
+                    respond +=  CharSet + " ";
+            }
+            if (Request.Form["decode"] != null)
+            {
+                string input = Request.Form["input"];
+                if (input.EndsWith(" "))
+                    input = input.Remove(input.Length - 1);
+                string[] codes = input.Split(' ');
+
+                char[] chars = new char[codes.Length];
+                for (int i = 0; i < codes.Length; i++)
+                {
+                    chars[i] = (char)int.Parse(codes[i]);
+                }
+
+                respond = new string(chars);
+
+
+            }
 
         }
     }
